160b207b062a5b773218e438707b93de1cab889c
pardhupardhu689-dell <pardhupardhu689@gmail.com>
2026-09-08 18:42:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (98.78%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        
+        // Step 1: Find the first decreasing element from right
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: Find the next greater element
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            // Step 3: Swap
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 4: Reverse the remaining part
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
